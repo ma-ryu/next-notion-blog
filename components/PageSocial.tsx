8986677f34eba20ecf0b818def210f755ce43efb
ma-ryu <54539186+ma-ryu@users.noreply.github.com>
2024-06-14 18:23:19 +0900

    [fix] x icon

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -1,10 +1,17 @@
-import * as React from 'react'
+import * as React from 'react';
 
-import cs from 'classnames'
 
-import * as config from '@/lib/config'
 
-import styles from './PageSocial.module.css'
+import cs from 'classnames';
+
+
+
+import * as config from '@/lib/config';
+
+
+
+import styles from './PageSocial.module.css';
+
 
 interface SocialLink {
   name: string
@@ -19,8 +26,15 @@ const socialLinks: SocialLink[] = [
     href: `https://twitter.com/${config.twitter}`,
     title: `Twitter @${config.twitter}`,
     icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
-        <path d='M23.44 4.83c-.8.37-1.5.38-2.22.02.93-.56.98-.96 1.32-2.02-.88.52-1.86.9-2.9 1.1-.82-.88-2-1.43-3.3-1.43-2.5 0-4.55 2.04-4.55 4.54 0 .36.03.7.1 1.04-3.77-.2-7.12-2-9.36-4.75-.4.67-.6 1.45-.6 2.3 0 1.56.8 2.95 2 3.77-.74-.03-1.44-.23-2.05-.57v.06c0 2.2 1.56 4.03 3.64 4.44-.67.2-1.37.2-2.06.08.58 1.8 2.26 3.12 4.25 3.16C5.78 18.1 3.37 18.74 1 18.46c2 1.3 4.4 2.04 6.97 2.04 8.35 0 12.92-6.92 12.92-12.93 0-.2 0-.4-.02-.6.9-.63 1.96-1.22 2.56-2.14z' />
+      <svg
+        xmlns='http://www.w3.org/2000/svg'
+        x='0px'
+        y='0px'
+        width='100'
+        height='100'
+        viewBox='0 0 50 50'
+      >
+        <path d='M 5.9199219 6 L 20.582031 27.375 L 6.2304688 44 L 9.4101562 44 L 21.986328 29.421875 L 31.986328 44 L 44 44 L 28.681641 21.669922 L 42.199219 6 L 39.029297 6 L 27.275391 19.617188 L 17.933594 6 L 5.9199219 6 z M 9.7167969 8 L 16.880859 8 L 40.203125 42 L 33.039062 42 L 9.7167969 8 z'></path>
       </svg>
     )
   },
